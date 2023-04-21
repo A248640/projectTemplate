6b2613765ad58ab7c6c8a1316ab1cdc18480cbfd
A248640 <128206045+A248640@users.noreply.github.com>
2023-04-22 00:46:40 +0300

    Add files via upload

---
+++ source/main.cpp
@@ -0,0 +1,10 @@
+// D:/gcc/bin/g++.exe -Wall -Wextra -Wpedantic -mconsole -O0 -g -std=c++20 -o build/main.exe source/main.cpp source/message.cpp -I header
+
+#include "message.h"
+
+int main()
+{
+    showMessage();
+
+    return 0;
+}
---
+++ source/message.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "message.h"
+
+int showMessage()
+{
+    std::cout << "Hello world!" << std::endl;
+
+    return 0;
+}
