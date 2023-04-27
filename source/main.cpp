65097af36a06d982740c316c178767162cc0a063
A248640 <128206045+A248640@users.noreply.github.com>
2023-04-27 20:23:11 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,8 @@
-// D:/gcc/bin/g++.exe -Wall -Wextra -Wpedantic -mconsole -O0 -g -std=c++20 -o build/main.exe source/main.cpp source/message.cpp -I header
+/*
+    g++.exe -Wall -Wextra -Wpedantic -mconsole -O0 -g -std=c++20 -o build/main.exe source/main.cpp source/message.cpp -I header
+*/
 
-#include "message.h"
+#include "message.hpp"
 
 int main()
 {
